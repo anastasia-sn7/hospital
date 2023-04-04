47ae02e704c7f7f4667e131097baa40534de7cb3
anastasia-sn7 <melaniefed03@gmail.com>
2023-04-04 23:38:03 +0300

    u

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "home#index"
   resources :med_certificates
   resources :clinics, :doctors, :medcards, :pacients, :repartments, :specializations
