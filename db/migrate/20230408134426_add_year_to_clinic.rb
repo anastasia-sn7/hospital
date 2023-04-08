2d04ed272e466035cdcf4d6426bb7af42c67253b
anastasia-sn7 <melaniefed03@gmail.com>
2023-04-08 16:52:06 +0300

    u

---
+++ db/migrate/20230408134426_add_year_to_clinic.rb
@@ -0,0 +1,5 @@
+class AddYearToClinic < ActiveRecord::Migration[7.0]
+  def change
+    add_column :clinics, :year, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_202116) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_08_134426) do
   create_table "clinics", force: :cascade do |t|
     t.integer "clinic_id"
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_202116) do
     t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "year"
     t.index ["clinic_id"], name: "index_clinics_on_clinic_id"
   end
 
